f90f03460fa8f0efe7ee22d1ed8e09785bf321fb
Shobana Mathiarul <mathairulshobana@gmail.com>
2023-08-12 20:58:44 +0200

    Added authentication using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Ecommerce store admin App',
-  description: 'Ecommerce store admin App',
-}
+  title: 'Ecommerce Admin',
+  description: 'Ecommerce Admin',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
