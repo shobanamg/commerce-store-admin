f5a8c81ebfa8d6799869b4bbd022828e7d8f65d9
Shobana Mathiarul <mathairulshobana@gmail.com>
2023-08-13 16:55:54 +0200

    added toast provider to the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { ModalProvider } from '@/providers/modal-provider';
+import { ToastProvider } from '@/providers/toast-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ModalProvider />
+          <ToastProvider />
           {children}
         </body>
       </html>
