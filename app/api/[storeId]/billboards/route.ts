c81fa37e5a0f46a9823023ed39e30262cea0e05b
Shobana Mathiarul <mathiarulshobana@gmail.com>
2023-08-20 13:34:50 +0200

    now we will be able to create, update and delete billboards

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -14,6 +14,8 @@ export async function POST(
 
     const { label, imageUrl } = body;
 
+    console.log('[BILLBOARDS_POST]', 'hi');
+
     if (!userId) {
       return new NextResponse('Unauthenticated', { status: 403 });
     }
