713788c7292e5f794bf970f951152f1c5c59940c
Shobana Mathiarul <mathiarulshobana@gmail.com>
2023-08-26 22:37:32 +0200

    Added crud endpoints for colors

---
+++ app/api/[storeId]/colors/[colorId]/route.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from 'next/server';
+
+import { isUserStore } from '@/lib/api-utils';
+import prismadb from '@/lib/prismadb';
+
+export async function GET(
+  req: Request,
+  { params }: { params: { colorId: string; storeId: string } }
+) {
+  try {
+    await isUserStore(params.storeId);
+
+    const color = await prismadb.color.findUnique({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log('[COLOR_GET]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { colorId: string; storeId: string } }
+) {
+  try {
+    await isUserStore(params.storeId);
+
+    const color = await prismadb.color.delete({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log('[COLOR_DELETE]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { colorId: string; storeId: string } }
+) {
+  try {
+    await isUserStore(params.storeId);
+
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse('Value is required', { status: 400 });
+    }
+
+    const color = await prismadb.color.update({
+      where: {
+        id: params.colorId,
+      },
+      data: {
+        name,
+        value,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log('[COLOR_PATCH]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
---
+++ app/api/[storeId]/colors/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse } from 'next/server';
+
+import { isUserStore } from '@/lib/api-utils';
+import prismadb from '@/lib/prismadb';
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    await isUserStore(params.storeId);
+
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse('Value is required', { status: 400 });
+    }
+
+    const color = await prismadb.color.create({
+      data: {
+        name,
+        value,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log('[COLORS_POST]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    await isUserStore(params.storeId);
+
+    const colors = await prismadb.color.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(colors);
+  } catch (error) {
+    console.log('[COLORS_GET]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
